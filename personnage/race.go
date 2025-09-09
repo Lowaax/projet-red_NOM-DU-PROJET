0049cc3ce087b8500f6cf0e77bccc2a0b396bdaf
Hyshax <jacques.lafolie@ynov.com>
2025-09-09 15:23:09 +0200

    base

---
+++ personnage/race.go
@@ -0,0 +1,27 @@
+package piscine
+
+type Race struct {
+	elf         string
+	nain        string
+	orc         string
+	human       string
+	dragon      string
+	mort_vivant string
+	ange        string
+	orque       string
+	centaure    string
+	fée         string
+	Lycanthrope string
+	farfadet    string
+	antromorphe string
+}
+
+type classes struct {
+	chevalier    string
+	sorcier      string
+	archer       string
+	assassin     string
+	prêtre       string
+	necromancien string
+	berserker    string
+}
