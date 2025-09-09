fa7846be04bb8223a4a0e65c49dbffa3f51b5120
Hyshax <jacques.lafolie@ynov.com>
2025-09-09 16:06:17 +0200

    Update main.go

--- main.go
+++ main.go
@@ -2,4 +2,18 @@ package main
 
 func initCharacter() {
 
+func main() {
+
+}
+
+func initCharacter(name string, race Race, class Classes, niveau int, pv_max int, pv_actuel int, inventaire []string) Character {
+	return Character{
+		Name:       name,
+		Race:       race,
+		Class:      class,
+		Niveau:     niveau,
+		Pv_max:     pv_max,
+		Pv_actuel:  pv_actuel,
+		Inventaire: inventaire,
+	}
 }
