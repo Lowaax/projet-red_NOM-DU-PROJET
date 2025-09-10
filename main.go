fba7f7921aa4a8cf80e84976c5b4f37f18867804
Lowaax <florin.hamelin@gmail.com>
2025-09-10 19:30:32 +0200

    Update main.go
    changement des dialogues

--- main.go
+++ main.go
@@ -105,12 +105,12 @@ func Menu(c projet.Character) {
 func Shop(c projet.Character) {
 	var choice_shop int
 	fmt.Println("===== Marchand =====")
-	fmt.Println("Marchand: 'Salut, gros fils de pute, \nqu'est ce qui t'intéresserait dans mon magasin de merde ?")
+	fmt.Println("Marchand: Bienvenue jeune aventurier, \nqu'est ce qui t'intéresserait dans ma boutique ?")
 	fmt.Println("===== Stock =====")
-	fmt.Println("1: Potion de vie de fdp (Gratuit)")
+	fmt.Println("1: Potion de vie (Gratuit)")
 	fmt.Println("0: Annuler")
 	fmt.Println("======================")
-	fmt.Println("Marchand: C'est tout ce que j'ai, sale merde de pigeon")
+	fmt.Println("Marchand: C'est tout ce que j'ai, jeune aventurier")
 	fmt.Println("Vous avez choisi: ")
 	fmt.Scan(&choice_shop)
 	if choice_shop == 1 {
@@ -119,7 +119,7 @@ func Shop(c projet.Character) {
 	} else if choice_shop == 0 {
 		fmt.Println("Marchand: Tu change d'avis ? bah dégage alors")
 	} else {
-		fmt.Println("T'es vraiment con toi, ", choice_shop, "n'est pas une option")
+		fmt.Println("Tu ne peux pas, ", choice_shop, " n'est pas disponible")
 	}
 }
 
